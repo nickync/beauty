08920d1b0c43de70817b16eaf7e2b7a2fe1b1c07
nickync <lizheng9024@gmail.com>
2023-11-28 22:22:08 -0500

    v2.21

--- beauty/src/comp/FooterComp.jsx
+++ beauty/src/comp/FooterComp.jsx
@@ -1,6 +1,6 @@
 export default function FooterComp() {
   return (
-    <div className="container-fluid d-flex justify-content-around bg-dark">
+    <div className="container-fluid d-flex justify-content-around bg-header">
         <div className="text-start fst-italic text-secondary">© 黑茶妹 2023</div>
         <div className="text-secondary fst-italic">Cell: 347-622-5571</div>
     </div>
