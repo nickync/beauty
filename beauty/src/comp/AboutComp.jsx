705196fed2327558f79d6bd25ac63dabd0b09ce0
nickync <lizheng9024@gmail.com>
2023-12-01 00:07:56 -0500

    v2.4

--- beauty/src/comp/AboutComp.jsx
+++ beauty/src/comp/AboutComp.jsx
@@ -3,11 +3,11 @@ import { Container, Row, Col } from "react-bootstrap"
 export default function AboutComp({aboutRef}) {
   return (
     <Container ref={aboutRef} className="text-dark bg-service" fluid>
-    <Row className="my-5">
+    <Row className="my-1">
         <h1>关于</h1>
     </Row>
-    <Row className="justify-content-center">
-        <Col>
+    <Row className="justify-content-center about">
+        <Col className="my-2">
             <Row className="justify-content-center">
                 个人简介
             </Row>
@@ -20,7 +20,10 @@ export default function AboutComp({aboutRef}) {
                 地址:
             </Row>
             <Row className="justify-content-center">
-                36-09 Main St (10.D) 缅街
+                <span className="mb-2">
+                    36-09 Main St (10.D) 缅街
+                </span>
+
                 {/* <iframe title='Map' className='pt-1 pb-1' src={} width="300" height="150" style={{"border":0}} loading="lazy" allowFullScreen='' referrerPolicy='no-referrer-whendowngrade' ></iframe>           */}
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d188.87311644243064!2d-73.83161840783758!3d40.762687569048765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2600ff2db78fb%3A0x420a7eeccff3967d!2s36-09%20Main%20St%2C%20Flushing%2C%20NY%2011354!5e0!3m2!1sen!2sus!4v1701405614185!5m2!1sen!2sus"
--- beauty/src/comp/FooterComp.jsx
+++ beauty/src/comp/FooterComp.jsx
@@ -1,6 +1,6 @@
 export default function FooterComp() {
   return (
-    <div className="container-fluid d-flex justify-content-around bg-header">
+    <div className="container-fluid d-flex justify-content-around bg-header mt-3">
         <div className="text-start fst-italic text-white">© 黑茶妹 2023</div>
         <div className="text-white fst-italic">Cell: 347-622-5571</div>
     </div>
